e5ccb9161cb88946d586bae66f58760dff7cac48
Your Name <you@example.com>
2016-04-17 10:44:12 +0000

    first commit

---
+++ src/main/java/test/test/demo/App.java
@@ -0,0 +1,13 @@
+package test.test.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
